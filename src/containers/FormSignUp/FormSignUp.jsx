29caa21f464b58c05ef9758141c784e79ec478ee
Gurov Dmitriy <gurovdmitriy1991@gmail.com>
2022-10-22 20:50:19 +0300

    refactor: sign up to functional

--- src/containers/FormSignUp/FormSignUp.jsx
+++ src/containers/FormSignUp/FormSignUp.jsx
@@ -1,87 +1,82 @@
-import React from "react"
 import InputBox from "../../components/InputBox/InputBox"
 import ButtonDefault from "../../components/ButtonDefault/ButtonDefault"
 import { configInput, configButton } from "./data"
 import api from "../../api"
 import "./styles.scss"
+import { useState } from "react"
 
-class FormSignUp extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      name: "",
-      email: "",
-      password: "",
-      passwordConfirm: "",
-    }
-  }
+function FormSignUp() {
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  })
 
-  handleSubmit = async (evt) => {
+  async function handleSubmit(evt) {
     evt.preventDefault()
+
     await api.auth.createUser({
-      email: this.state.email,
-      password: this.state.password,
+      email: state.email,
+      password: state.password,
     })
   }
 
-  handleInput = (evt, { name }) => {
-    this.setState({
+  function handleInput(evt, { name }) {
+    setState({
+      ...state,
       [name]: evt.target.value,
     })
   }
 
-  render() {
-    return (
-      <div className="form-sign-up">
-        <h3 className="form-sign-up__caption">I dont have an account</h3>
-        <p className="form-sing-up__description">
-          Sing up with your email and password
-        </p>
+  return (
+    <div className="form-sign-up">
+      <h3 className="form-sign-up__caption">I dont have an account</h3>
+      <p className="form-sing-up__description">
+        Sing up with your email and password
+      </p>
 
-        <form
-          className="form-sign-up__form"
-          action=""
-          method="POST"
-          onSubmit={this.handleSubmit}
-        >
-          <InputBox
-            onInput={(evt) => {
-              this.handleInput(evt, { name: "name" })
-            }}
-            value={this.state.name}
-            dataItem={configInput.name}
-          />
-          <InputBox
-            onInput={(evt) => {
-              this.handleInput(evt, { name: "email" })
-            }}
-            value={this.state.email}
-            dataItem={configInput.email}
-          />
-          <InputBox
-            onInput={(evt) => {
-              this.handleInput(evt, { name: "password" })
-            }}
-            value={this.state.password}
-            dataItem={configInput.password}
-          />
-          <InputBox
-            onInput={(evt) => {
-              this.handleInput(evt, { name: "passwordConfirm" })
-            }}
-            value={this.state.passwordConfirm}
-            dataItem={configInput.passwordConfirm}
-          />
+      <form
+        className="form-sign-up__form"
+        action=""
+        method="POST"
+        onSubmit={handleSubmit}
+      >
+        <InputBox
+          onInput={(evt) => {
+            handleInput(evt, { name: "name" })
+          }}
+          value={state.name}
+          dataItem={configInput.name}
+        />
+        <InputBox
+          onInput={(evt) => {
+            handleInput(evt, { name: "email" })
+          }}
+          value={state.email}
+          dataItem={configInput.email}
+        />
+        <InputBox
+          onInput={(evt) => {
+            handleInput(evt, { name: "password" })
+          }}
+          value={state.password}
+          dataItem={configInput.password}
+        />
+        <InputBox
+          onInput={(evt) => {
+            handleInput(evt, { name: "passwordConfirm" })
+          }}
+          value={state.passwordConfirm}
+          dataItem={configInput.passwordConfirm}
+        />
 
-          <div className="form-sign-up__button-box">
-            <ButtonDefault dataItem={configButton.signUp}>
-              Sign Up
-            </ButtonDefault>
-          </div>
-        </form>
-      </div>
-    )
-  }
+        <div className="form-sign-up__button-box">
+          <ButtonDefault dataItem={configButton.signUp}>Sign Up</ButtonDefault>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default FormSignUp
