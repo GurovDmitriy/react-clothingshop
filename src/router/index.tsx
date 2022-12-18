9a53f8cc7c651fbd97df3fd1a61314f24003e9a4
Gurov Dmitriy <gurovdmitriy1991@gmail.com>
2022-12-18 22:22:58 +0300

    test: deploy

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,84 +1,78 @@
-// import { lazy } from "react"
+import { lazy } from "react"
 import { createHashRouter } from "react-router-dom"
-// import { fetchCategory as rootLoader } from "../api/category/category"
-import { fetchCollectionList as collectionLoader } from "../api/shop/shop"
+import { fetchCategory as rootLoader } from "../api/category/category"
+import {
+  fetchCollectionList as collectionLoader,
+  fetchShopItems as shopLoader,
+} from "../api/shop/shop"
 import AboutLayout from "../layouts/AboutLayout/AboutLayout"
 import HomeLayout from "../layouts/HomeLayout/HomeLayout"
-import AboutPage from "../pages/AboutPage/AboutPage"
-import CartPage from "../pages/CartPage/CartPage"
-import CategoryPage from "../pages/CategoryPage/CategoryPage"
-import ContactPage from "../pages/ContactPage/ContactPage"
 import ErrorPage from "../pages/ErrorPage/ErrorPage"
 import HomePage from "../pages/HomePage/HomePage"
-import ShopPage from "../pages/ShopPage/ShopPage"
-import SignPage from "../pages/SignPage/SignPage"
 
-// const AboutPage = lazy(() => import("../pages/AboutPage/AboutPage"))
-// const CartPage = lazy(() => import("../pages/CartPage/CartPage"))
-// const CategoryPage = lazy(() => import("../pages/CategoryPage/CategoryPage"))
-// const ContactPage = lazy(() => import("../pages/ContactPage/ContactPage"))
-// const ShopPage = lazy(() => import("../pages/ShopPage/ShopPage"))
-// const SignPage = lazy(() => import("../pages/SignPage/SignPage"))
+const AboutPage = lazy(() => import("../pages/AboutPage/AboutPage"))
+const CartPage = lazy(() => import("../pages/CartPage/CartPage"))
+const CategoryPage = lazy(() => import("../pages/CategoryPage/CategoryPage"))
+const ContactPage = lazy(() => import("../pages/ContactPage/ContactPage"))
+const ShopPage = lazy(() => import("../pages/ShopPage/ShopPage"))
+const SignPage = lazy(() => import("../pages/SignPage/SignPage"))
 
-const router = createHashRouter(
-  [
-    {
-      path: "/",
-      element: <HomeLayout />,
-      errorElement: <ErrorPage />,
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <ErrorPage />,
 
-      children: [
-        {
-          index: true,
-          element: <HomePage />,
-          // loader: rootLoader,
-        },
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+        loader: rootLoader,
+      },
 
-        {
-          path: "shop",
-          element: <ShopPage />,
-          // loader: shopLoader,
-        },
+      {
+        path: "shop",
+        element: <ShopPage />,
+        loader: shopLoader,
+      },
 
-        {
-          path: "shop/:category",
-          element: <CategoryPage />,
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore
-          loader: collectionLoader,
-        },
+      {
+        path: "shop/:category",
+        element: <CategoryPage />,
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        loader: collectionLoader,
+      },
 
-        {
-          path: "sign",
-          element: <SignPage />,
-        },
+      {
+        path: "sign",
+        element: <SignPage />,
+      },
 
-        {
-          path: "cart",
-          element: <CartPage />,
-        },
-      ],
-    },
+      {
+        path: "cart",
+        element: <CartPage />,
+      },
+    ],
+  },
 
-    {
-      path: "/about",
-      element: <AboutLayout />,
-      errorElement: <ErrorPage />,
+  {
+    path: "/about",
+    element: <AboutLayout />,
+    errorElement: <ErrorPage />,
 
-      children: [
-        {
-          index: true,
-          element: <AboutPage />,
-        },
+    children: [
+      {
+        index: true,
+        element: <AboutPage />,
+      },
 
-        {
-          path: "contact",
-          element: <ContactPage />,
-        },
-      ],
-    },
-  ],
-  { basename: process.env.PUBLIC_URL }
-)
+      {
+        path: "contact",
+        element: <ContactPage />,
+      },
+    ],
+  },
+])
 
 export default router
