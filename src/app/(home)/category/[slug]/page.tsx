75467a0d28d314890d07c0ddada76639092ed7e7
GurovDmitriy <gurovdmitriy1991@gmail.com>
2024-01-04 01:42:56 +0300

    feat: static params

--- src/app/(home)/category/[slug]/page.tsx
+++ src/app/(home)/category/[slug]/page.tsx
@@ -1,11 +1,20 @@
 import { ContainerCategoryDetail } from "@/domain/Category/_domain/CategoryDetail/ContainerCategoryDetail"
 import { SkeletonCardCategory } from "@/domain/Category/_domain/CategoryAll/components/SkeletonCardCategory/SkeletonCardCategory"
 import { Suspense } from "react"
+import { actionCategoryPreview } from "@/domain/Category/_domain/CategoryPreview/actions/actionCategoryPreview"
 
 interface IProps {
   params: {
     slug: string
   }
+}
+
+export async function generateStaticParams() {
+  const categories = await actionCategoryPreview()
+
+  return categories.map((category) => ({
+    slug: category.value,
+  }))
 }
 
 export default function PageCategory(props: IProps) {
